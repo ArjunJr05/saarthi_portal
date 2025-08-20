6670eaf062eb269086f23188703fab921c63133b
ArjunJr05 <arjunfree256@gmail.com>
2025-08-20 18:40:48 +0530

    final updation2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c65bd6ed7c6bfeac433e5fc6d0da8cfc
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b6839a21c92f20e58aef91158f37fd3a",
+"flutter_bootstrap.js": "7de3cbe18cfd4ec56b246850c78e0f24",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
